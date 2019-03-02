29d067caa13176c27ca2372e5d19dca8a9ce1ece
liuwill <liuwei_will@qq.com>
2019-03-02 17:24:05 +0800

    reade csv file success

---
+++ README.md
@@ -0,0 +1,1 @@
+# csv-reducer-swift
--- csv-reducer/HomeViewController.swift
+++ csv-reducer/HomeViewController.swift
@@ -10,6 +10,12 @@ import Cocoa
 
 class HomeViewController: NSViewController {
 
+    @IBOutlet weak var fileButton: NSButton!
+    @IBOutlet weak var filenameField: NSTextField!
+    @IBOutlet weak var lineField: NSTextField!
+    
+    var filePath = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,6 +28,32 @@ class HomeViewController: NSViewController {
         }
     }
 
-
+    @IBAction func chooseCsvFile(_ sender: NSButton) {
+        let openPanel = NSOpenPanel()
+        openPanel.allowsMultipleSelection = false
+        openPanel.canChooseDirectories = false
+        openPanel.canCreateDirectories = false
+        openPanel.canChooseFiles = true
+        openPanel.allowedFileTypes = ["csv"]
+        openPanel.title = "选择一个CSV文件"
+        openPanel.beginSheetModal(for:self.view.window!) { (response) in
+            if response.rawValue == NSFileHandlingPanelOKButton {
+                let fileUrl = openPanel.url
+                let selectedPath = openPanel.url!.path
+                // do whatever you what with the file path
+                
+                self.filePath = selectedPath
+                print("CSV SELECTED: " + selectedPath)
+                
+                self.filenameField.stringValue = selectedPath
+//                let  readHandle = NSFileHandle.init(forReadingAtPath: selectedPath)
+                let fileContent = try! String.init(contentsOfFile: selectedPath)
+                let contentLines = fileContent.split(separator: "\n")
+                self.lineField.stringValue = String(contentLines.count)
+            }
+            openPanel.close()
+        }
+    }
+    
 }
 
