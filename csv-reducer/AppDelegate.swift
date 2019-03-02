de0ee6429a9acd9fc4b889b97af9b90e13c0add4
liuwill <liuwei_will@qq.com>
2019-03-02 15:03:23 +0800

    Initial Commit

---
+++ csv-reducer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  csv-reducer
+//
+//  Created by videopls on 2019/3/2.
+//  Copyright © 2019 liuwill. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ csv-reducer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  csv-reducer
+//
+//  Created by videopls on 2019/3/2.
+//  Copyright © 2019 liuwill. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
