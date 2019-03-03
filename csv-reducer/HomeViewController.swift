73dfac1c7228caecb1dd931601095886242d35d2
liuwill <liuwei_will@qq.com>
2019-03-03 15:40:26 +0800

    split and save csv file success

--- csv-reducer/HomeViewController.swift
+++ csv-reducer/HomeViewController.swift
@@ -17,7 +17,8 @@ class HomeViewController: NSViewController {
 
     @IBOutlet weak var targetSizeField: NSTextField!
     @IBOutlet weak var targetColumnField: NSTextField!
-
+    @IBOutlet weak var targetIdField: NSTextField!
+    
     var filePath = ""
 
     override func viewDidLoad() {
@@ -33,44 +34,95 @@ class HomeViewController: NSViewController {
     }
 
     @IBAction func handleFileSplit(_ sender: Any) {
+        let openPanel = NSOpenPanel()
+        openPanel.allowsMultipleSelection = false
+        openPanel.canChooseDirectories = true
+        openPanel.canCreateDirectories = false
+        openPanel.canChooseFiles = false
+        openPanel.title = "选择保存目录"
+        openPanel.beginSheetModal(for:self.view.window!) { (response) in
+            if response == NSApplication.ModalResponse.OK {
+                self.writeDownTargetFile(fileUrl: openPanel.url)
+            }
+            openPanel.close()
+        }
+    }
+    
+    func alertMessage(title: String, message: String) {
+        let alert: NSAlert = NSAlert()
+        alert.messageText = title
+        alert.informativeText = message
+        alert.alertStyle = NSAlert.Style.warning
+        alert.addButton(withTitle: "OK")
+        alert.runModal()
+    }
+    
+    func writeDownTargetFile (fileUrl: URL?) {
         if(self.filePath.count < 1) {
-            let alert: NSAlert = NSAlert()
-            alert.messageText = "未选择文件"
-            alert.informativeText = "请先选择一个CSV文件"
-            alert.alertStyle = NSAlert.Style.warning
-            alert.addButton(withTitle: "OK")
-            alert.runModal()
+            self.alertMessage(title: "未选择文件", message: "请先选择一个CSV文件")
             return
         }
-
-        var columnCount:Int? = 0
-        var fileSize:Int? = 2000
+        
+        let filenameData = self.filePath.split(separator: "/")
+        let sourcePath = filenameData[0...filenameData.count - 2].joined(separator: "/")
+        
+        var startId: Int32 = 10000
+        var columnCount: Int32 = 2
+        var fileSize: Int32 = 5000
         
         if (self.targetColumnField.stringValue.count > 0) {
-            columnCount = Int(self.targetColumnField.stringValue)
+            columnCount = self.targetColumnField.intValue
         }
         if (self.targetSizeField.stringValue.count > 0) {
-            fileSize = Int(self.targetSizeField.stringValue)
+            fileSize = self.targetSizeField.intValue
         }
-
+        
         let fileContent = try! String.init(contentsOfFile: self.filePath)
         let contentLines = fileContent.split(separator: "\n")
-
-        let titleLine = contentLines[0]
         
-        let singleFileContent = ""
-        let tmpDir = NSTemporaryDirectory()
-        let fileMap: [String: String]
+        let titleStr = contentLines[0]
+        let titleLine = contentLines[0].split(separator: ",")
+        if (titleLine.count < columnCount - 1) {
+            self.alertMessage(title: "文件格式不正确", message: "文件没有指定索引列")
+            return
+        }
+        
+        //        let singleFileContent = ""
+        //        let tmpDir = NSTemporaryDirectory()
+        //        let fileMap: [String: String]
         
         var count = 0
         for singleLine in contentLines {
-            if count == 0{
+            if count == 0 {
+                count += 1
                 continue
             }
-            let lineData = singleLine.split(separator: ",")
-
             
             count += 1
+            let lineData = singleLine.split(separator: ",")
+            let idVal = lineData[1]
+            let idIntVal = Int32(idVal)
+            let fileNo = (idIntVal ?? 0 - startId) / fileSize
+            let lineFileName = fileUrl!.path + "/piece-" + String(fileNo) + ".csv"
+            
+            //            print(lineFileName)
+            let fileURL = URL(fileURLWithPath: lineFileName)
+            let fileManager = FileManager.default
+            if !fileManager.fileExists(atPath: lineFileName) {
+                let writeTitle = titleStr + "\n"
+                let createSuccess = fileManager.createFile(atPath: lineFileName,contents:writeTitle.data(using: .utf8),attributes:nil)
+                print("FILE Create: : \(createSuccess): " + lineFileName)
+            }
+            
+            let writeLine = singleLine + "\n"
+            do {
+                let fileHandle = try FileHandle(forWritingTo: fileURL)
+                fileHandle.seekToEndOfFile()
+                fileHandle.write(writeLine.data(using: .utf8)!)
+                fileHandle.closeFile()
+            } catch {
+                print("Error writing to file \(error)")
+            }
         }
     }
 
@@ -84,7 +136,7 @@ class HomeViewController: NSViewController {
         openPanel.title = "选择一个CSV文件"
         openPanel.beginSheetModal(for:self.view.window!) { (response) in
             if response == NSApplication.ModalResponse.OK {
-//                let fileUrl = openPanel.url
+                let fileUrl = openPanel.url
                 let selectedPath = openPanel.url!.path
                 // do whatever you what with the file path
                 
